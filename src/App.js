703cfddc928ed558223b422bb961c3088411a71f
Bhama <bhamaguruswami@gmail.com>
2022-10-17 14:57:28 +0200

    one pageappl

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
 import './App.css';
+import { useState } from 'react';
+import React  from 'react';
 import Home from './components/Home';
-import Work from './components/Work';
 import Story from './components/Story';
-import Contact from './components/Contact';
-import { BrowserRouter as Router,Route, Routes} from "react-router-dom"
-import  React from 'react';
+import Work from './components/Work';
+import {Row,Col} from 'react-bootstrap/';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Image from './img/myphoto.jpg'
+
 function App() {
+  const[myPage, setMyPage] = useState("Home");
+  const [isMobile,setIsMobile]= useState(false)
+
   return (
-    <div className="App">
-      <Router>
-      <Routes>
-      <Route exact path="/" element={<Home />}/>
-      <Route path='/work' element={<Work />}/>
-      <Route path='/story' element={<Story />}/>
-      <Route path='/contact' element={<Contact />}/> 
-      </Routes>
-      </Router>
+    <div className="App ">
+    <Box sx={{ flexGrow: 1 }}>
+    <Grid container>
+      <Grid  className='box1' item xs={12} md={6} lg={6}>
+        <Row className='fullwidth'>
+          <Col >
+          <div className='container'>
+            <nav className='navbar'>
+              <div><h3 className='logo'> <strong>bh</strong><span className="heading-dot"></span></h3></div>
+              <ul className={isMobile? 'nav-links-mobile':'nav-links'}
+              onClick={()=>setIsMobile(false)}>
+                <li className='home' onClick={()  => setMyPage("Home")}>Home</li >
+                <li className='story' onClick={()  => setMyPage("Story")}>Story</li>
+                <li  className='work' onClick={()  => setMyPage("Work")}>Work</li>
+              </ul>
+              <button className='mobile-menu-icon'
+              onClick={()=>setIsMobile(!isMobile) }
+              >
+              {isMobile ? <i className="fa-sharp fa-solid fa-circle-xmark"></i>:<i className="fa-solid fa-bars"></i>}
+              </button>
+            </nav>
+            <div>
+              {myPage === "Home" && <Home/>}
+              {myPage === "Story" && <Story/>}
+              {myPage === "Work" && <Work/>}
+            </div>
+            </div>
+          </Col>
+        </Row>
+      </Grid>
+
+
+      <Grid item xs={6} md={6}  lg={6}>
+        <Col className='hide-on-mobile lg-6'>
+          <img src={Image} className="profile" alt="profile" />
+        </Col>
+      </Grid>
+    </Grid>
+  </Box>
     </div>
   );
 }
+
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,104 +1,25 @@
-import {Row,Col} from 'react-bootstrap/';
+import { Row, Col } from 'react-bootstrap/';
 import React, { useState } from 'react'
 import Image from '../img/myphoto.jpg'
-import  './Home.css'
-import {FaGithub,FaInstagram,FaLinkedinIn,} from 'react-icons/fa';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import {Link} from "react-router-dom"
-import { Diversity1TwoTone } from '@mui/icons-material';
+import '../App.css'
+import { FaGithub, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+
 
 function Home() {
-  const [isMobile,setIsMobile]= useState(false)
+  const [isMobile, setIsMobile] = useState(false)
   return (
-    <Box sx={{ flexGrow: 1, textAlign: "center" }}>
-    <Grid container>
-      <Grid item xs={12} md={6} lg={6}>
-        <Row className='fullwidth'>
-          <Col>
-            <nav className='navbar'>
-              <h3 className='logo'> <strong>bh</strong>  <span class="heading-dot"></span></h3>
-              <ul className={isMobile? 'nav-links-mobile':'nav-links'}
-              onClick={()=>setIsMobile(false)}
-              >
-                {/* <Link to='/' className='home'>
-                  <li>Home</li>
-                </Link> */}
-                <Link to='./Work' className='work'>
-                  <li>Work</li> 
-                </Link>
-                <Link to='./Story' className='story'>
-                  <li>Story</li>
-                </Link>
-                <Link to='./Contact' className='contact'>
-                  <li>Contact</li>
-                </Link>
-
-              </ul>
-              <button className='mobile-menu-icon'
-              onClick={()=>setIsMobile(!isMobile) }
-              >
-              {isMobile ? <i className="fa-sharp fa-solid fa-circle-xmark"></i>:<i className="fa-solid fa-bars"></i>}
-              </button>
-            </nav>
-           <div className='intro'>
-            <h1 className='firstname'>bhama </h1>
-            <h3 className='secondname'>Guruswami <span class="dot"></span></h3>
-            <p>Hi, I’m Bhama Guruswami Junior fronted developer at <b>BeCode</b> and looking for an internship from November to February 2023</p>
-            <div className='social'>
-              <button  href=' https://github.com/BhamaGuruswami?tab=repositories' className='hello'>Say Hello</button>
-              <button className='github' ><FaGithub /></button>
-              <button className='instagram'>< FaInstagram /></button>
-                            {/* <button className='github' ><a href="https://github.com/BhamaGuruswami"  target="_blank"><FaGithub /></a></button> */}
-
-            </div>
-            </div>
-
-          </Col>
-        </Row>
-      </Grid>
-      <Grid item xs={6} md={6}  lg={6}>
-        <Col className='hide-on-mobile lg-6'>
-          <img src={Image} className="profile" alt="profile" />
-
-        </Col>
-      </Grid>
-    </Grid>
-  </Box>
-
-    // <Container>
-    //   <Row className='mx-auto'>
-    //     <Col className='lg-6 md-6' >
-    //     <Navbar expand="lg my-3">
-    //     <Navbar.Brand href="#home bg-danger"> <strong>bh</strong>  <small><FaCircle /></small>
-    //     </Navbar.Brand>
-    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //     <Navbar.Collapse id="basic-navbar-nav">
-    //       <Nav className="text-align-justify">
-    //         <Nav.Link to="./Home.js">Work</Nav.Link>
-    //         <Nav.Link href="./Story.js">Story</Nav.Link>
-    //         <Nav.Link href="./Contact.js">Contact</Nav.Link>
-    //       </Nav>
-    //     </Navbar.Collapse>
-    //    </Navbar>
-    //     <h1>Bhama</h1>
-    //     <h2>Guruswami</h2>
-    //     <p>Hi, I’m Bhama Guruswami Junior fronted developer at <b>BeCode</b>.</p>
-    //     <Col className='social'>
-    //       <button className='hello'>Say Hello</button>
-    //       <button className='github'><FaGithub /></button>
-    //       <button className='instagram'>< FaInstagram/></button>
-    //       <button className='linkedin'>< FaLinkedinIn/></button>
-    //       </Col>
-
-    //     </Col> 
-    //     <Col className=' lg-6'>
-    //          <img src={Image} className="profile" alt="profile" />
-
-    //     </Col>
-
-    //   </Row>
-    // </Container>
+    // <div className='container'>
+      <div className='intro'>
+        <div><h1 className='firstname'>bhama </h1></div>
+        <div><h3 className='secondname'>Guruswami <span className="dot"></span></h3></div>
+        <div><p>Hi, I’m Bhama Guruswami Junior fronted developer at <b>BeCode</b> and looking for an internship from November to February 2023</p> </div>
+        <div className='social'>
+          <button href=' https://github.com/BhamaGuruswami?tab=repositories' className='hello'>Say Hello</button>
+          <button className='github' ><FaGithub /></button>
+          <button className='instagram'>< FaInstagram /></button>
+        </div>
+      </div>
+    // </div>
 
   );
 }
